2e18380de4776a6857b9bcd588481ef67cf382ad
Michel Alexandre Salim <michel@michel-slm.name>
2015-10-24 11:16:28 +0700

    edfh prop #1

--- test_edfh.py
+++ test_edfh.py
@@ -5,8 +5,10 @@ from hypothesis.strategies import integers
 def csum_to(n):
     if n == 0:
         return [n]
-    if n == 1:
+    elif n == 1:
         return [0, 1]
+    else:
+        return [0] * (n+1)
 
 
 @given(integers(min_value=0))
