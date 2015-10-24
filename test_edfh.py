cf8bb8abdd136fcee5fcca6cf8d74f17eb65edde
Michel Alexandre Salim <michel@michel-slm.name>
2015-10-24 11:18:31 +0700

    edfh prop #2

--- test_edfh.py
+++ test_edfh.py
@@ -3,15 +3,10 @@ from hypothesis.strategies import integers
 
 
 def csum_to(n):
-    if n == 0:
-        return [n]
-    elif n == 1:
-        return [0, 1]
-    else:
-        return [0] * (n+1)
+    return [sum(range(i)) for i in range(n+1)]
 
 
-@given(integers(min_value=0))
+@given(integers(min_value=0, max_value=100))
 def test_csum_prop(n):
     s_n = csum_to(n)
     assert len(s_n) == n+1
