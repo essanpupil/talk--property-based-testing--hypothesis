bbac9ac7128f80597c80bc36a07dfffd7877e319
Michel Alexandre Salim <michel@michel-slm.name>
2015-10-24 11:14:43 +0700

    edfh prop #0

--- test_edfh.py
+++ test_edfh.py
@@ -1,9 +1,17 @@
+from hypothesis import given
+from hypothesis.strategies import integers
+
+
 def csum_to(n):
     if n == 0:
         return [n]
     if n == 1:
         return [0, 1]
 
-def test_csum_01():
-    assert csum_to(0) == [0]
-    assert csum_to(1) == [0, 1]
+
+@given(integers(min_value=0))
+def test_csum_prop(n):
+    s_n = csum_to(n)
+    assert len(s_n) == n+1
+    assert s_n[0] == 0
+    assert s_n[-1] == sum(range(n+1))
